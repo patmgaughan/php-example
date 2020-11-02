81fc9b3a7b16711d2e02d3671dbd1616b5fbe12c
patmgaughan <71145712+patmgaughan@users.noreply.github.com>
2020-11-02 17:33:42 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>PHP Demo</title>
+</head>
+
+<body>
+
+	<h1>My First PHP Page</h1>
+	<?php
+		echo "string";
+	?>
+
+</body>
+</html>
+
+
+<!-- must be run on a server -->
